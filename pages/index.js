37b4536fbad1987a034fd501e613effdf66189f5
Mebrahtu Zemui Tesfazghi <mebrahtu.tesfazghi@divio.ch>
2026-02-24 23:56:56 +0100

    Test CI/CD deployment workflow

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Home = () => (
   <div>
     <h1>Welcome to Next.js!</h1>
+    <p>This is a sample page to test the GitHub Actions CI/CD workflow for a Next.js application.</p>
   </div>
 );
 
